096900a70b57d2a750025df83411a582b28c2d01
Gregory <kcils360@gmail.com>
2019-08-06 18:59:00 -0700

    Update server.js

--- curriculum/class-09/starter/server.js
+++ curriculum/class-09/starter/server.js
@@ -118,7 +118,7 @@ function getWeather(request, response) {
           // weatherCache(weatherSummaries, request.query.data.id);
           const SQL = `INSERT INTO weathers (forecast, time, location_id) VALUES ($1, $2, $3);`;
         weatherSummaries.forEach(summary => {
-          const values = [summary.forecast, summary.time, id];
+          const values = [summary.forecast, summary.time, request.query.data.id];
         client.query(SQL, values);
       })
           response.send(weatherSummaries);
